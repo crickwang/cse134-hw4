9a4b2e996ff9d834e1f612f7e8d7d7689cb3b0a1
Yifei Wang <yiw085@ucsd.edu>
2023-03-02 18:19:27 -0800

    final

--- blog.js
+++ blog.js
@@ -22,82 +22,112 @@ export function call()
 
 export function call1()
 {
+    let edit = document.createElement("button");
+    let edit_text = document.createTextNode("edit");
+    edit.appendChild(edit_text);
+    let remove = document.createElement("button");
+    let remove_text = document.createTextNode("remove");
+    remove.appendChild(remove_text);
+
     let node = document.createElement("li");
-    let textNode = document.createTextNode(`Blog ${arr.length + 1} is ${title.value}(${date.value}): ${sum.value}`);
+    let textNode = document.createTextNode(`${title.value}(${date.value}): ${sum.value}`);
     title.value = '';
     date.value = '1999-01-02';
     sum.value = '';
     node.appendChild(textNode);
+    node.appendChild(edit);
+    node.appendChild(remove);
     out.appendChild(node);
+
+    alert(edit.innerHTML);
+
     node.setAttribute("id", `div${arr.length}`);
+    edit.setAttribute("id", `edit${arr.length}`);
+    remove.setAttribute("id", `remove${arr.length}`);
+
+    edit.addEventListener('click', edi);
+    remove.addEventListener('click', remo);
+    localStorage.setItem(arr.length, JSON.stringify(node));
     arr.push(node);
-    console.log(arr);
-    localStorage.setItem('lists', JSON.stringify(arr));
+    
 }
 
-let choice = document.getElementById("input1");
-let button1 = document.getElementById("remove");
+
+
 let el1 = document.getElementById("d2");
-window.addEventListener("DOMContentLoaded", rm);
 let save1 = document.getElementById("conf2");
-
-export function rm()
-{
-    button1.addEventListener('click', remo);
-}
+let ind = 0;
 
 export function remo()
 {
     el1.showModal();
+    for (let i = 0; i < arr.length; i++)
+    {
+        
+        if (parseInt(this.id.split('remove')[1]) == i)
+        {
+            ind = i;
+            break;
+        }
+    }
     save1.addEventListener('click', rm_confirm);
 }
 
 export function rm_confirm()
 {
-    let ind = choice.value - 1;
     let node = arr[ind]
     node.remove();
     arr.splice(ind, 1);
+    ind = 0;
     for(let i = 0; i < arr.length; i++)
     {
         node = arr[i];
-        let temp = node.innerText.split("is");
-        node.innerText = `Blog ${i + 1} is${temp[1]}`;
+        for (let child of node.childNodes)
+        {
+            if(child.innerText == "edit")
+            {
+                child.setAttribute("id", `edit${i}`);
+            }
+            if(child.innerText == "remove")
+            {
+                child.setAttribute("id", `remove${i}`);
+            }
+        }
+        node.setAttribute("id", `div${i}`);
+        localStorage.setItem(i, JSON.stringify(node));
     }
-    console.log(arr);
-    localStorage.setItem('lists', JSON.stringify(arr));
+
+    localStorage.removeItem(arr.length);
 }
 
-let choice1 = document.getElementById("input2");
-let button2 = document.getElementById("edit");
+
 let el2 = document.getElementById("d3");
-window.addEventListener("DOMContentLoaded", ed);
 let save2 = document.getElementById("save3");
 let title1 = document.getElementById("title2");
 let date1 = document.getElementById("date2");
 let sum1 = document.getElementById("sum2");
 
-export function ed()
-{
-    button2.addEventListener('click', edi);
-}
-
 export function edi()
 {
     el2.showModal();
+    for (let i = 0; i < arr.length; i++)
+    {
+        if (parseInt(this.id.split('edit')[1]) == i)
+        {
+            ind = i;
+            break;
+        }
+    }
     save2.addEventListener('click', ed_confirm);
 }
 
 export function ed_confirm()
 {
-    let ind = choice1.value - 1;
     let node = arr[ind]
-    let temp = node.innerText.split("is");
-    node.innerText = `${temp[0]}is ${title1.value}(${date1.value}): ${sum1.value}`;
+    let newNode = `${title1.value}(${date1.value}): ${sum1.value}`;
+    node.firstChild.replaceWith(newNode);
     title1.value = '';
     date1.value = '1999-01-02';
     sum1.value = '';
-    arr[ind] = node;
-    console.log(arr);
-    localStorage.setItem('lists', JSON.stringify(arr));
+    localStorage.setItem(ind, JSON.stringify(node));
 }
